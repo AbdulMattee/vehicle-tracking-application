cba35607d375dc88cb27d63707b78c4ffc18ac60
AbdulMattee <97681961+Abdul-Mattee@users.noreply.github.com>
2022-10-12 17:19:48 +0500

    removed consoles

--- src/components/routes/PrivateRoute.jsx
+++ src/components/routes/PrivateRoute.jsx
@@ -3,7 +3,6 @@ import { Navigate } from 'react-router-dom'
 import userContext from '../context/Context'
 const PrivateRoute = ({ Component }) => {
   const user = useContext(userContext)
-  console.log(user)
   return (
     <>
       {(!user || !user.token) && <Navigate to="/login" />}
